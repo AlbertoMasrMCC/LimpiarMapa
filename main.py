016744a074da36d7056d27319654f3db564a14cc
Manuel Alberto Sotelo Rivas <manuel.sr@culiacan.tecnm.mx>
2022-12-31 02:11:46 -0700

    Mejorando la precisión para encontrar las esquinas de las calles

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import xml.etree.ElementTree as ET
 import csv
 
+# Validamos si la esquina se encuentra en una calle distinta a la que se esta analizando
+def validateCorner(root, name, corner_id):
+    for element in root:
+        if element.tag == "way":
+            for tag in reversed(element):
+                if tag.tag == "tag" and tag.attrib['k'] == "name":
+                    if tag.attrib["v"] == name:
+                        break
+                if tag.tag == "tag" and tag.attrib['k'] == "service":
+                    break
+                if tag.tag == "tag" and tag.attrib['k'] == "highway":
+                    # Recorrer todos los nodos del elemento "way"
+                    for nd in element:
+                        if nd.tag == "nd" and nd.attrib["ref"] == corner_id:
+                            return True
+    return False
+
 if __name__ == '__main__':
 
     # Convertir el archivo OSM a XML
-    tree = ET.parse("Culiacán Rosales.osm")
+    tree = ET.parse("Sector Centro.osm")
     root = tree.getroot()
 
     # Diccionario para almacenar las esquinas de cada calle
@@ -31,8 +48,9 @@ if __name__ == '__main__':
                     # Recorre todos los elementos "nd" (nodos) del elemento "way"
                     for nd in element:
                         if nd.tag == "nd":
-                            # Añade el identificador del nodo a la lista de esquinas de la calle
-                            corners[name].append(nd.attrib["ref"])
+                            # Si el nodo no está en la lista de esquinas de la calle, lo añade
+                            if validateCorner(root, name, nd.attrib["ref"]):
+                                corners[name].append(nd.attrib["ref"])
 
     # Iterar e imprimir cada nombre de la esquina y sus esquinas
     for name, corner_ids in corners.items():
@@ -41,16 +59,20 @@ if __name__ == '__main__':
             # Recorrer todos los nodos del árbol XML
             for element in root:
                 if element.tag == "node" and element.attrib["id"] == corner_id:
-                    for tag in element:
-                        if tag.tag == "tag" and tag.attrib['k'] == "highway":
-                            street_corners.append([element.attrib["lat"], element.attrib["lon"], "", "", name])
+                    street_corners.append([element.attrib["lat"], element.attrib["lon"], "", "", name])
 
-    # Recorre la lista de registros
     for i, record in enumerate(street_corners):
-        # Asigna las coordenadas al registro anterior (si existe)
+        # Si el índice del elemento actual es mayor que 0 (es decir, si no es el primer elemento de la lista)
         if i > 0:
-            street_corners[i - 1][2] = record[0]
-            street_corners[i - 1][3] = record[1]
+            # Si el nombre de la calle del elemento actual es igual al nombre de la calle del elemento anterior
+            if record[4] == street_corners[i - 1][4]:
+                # Asigna las coordenadas del elemento actual a la latitud y longitud final del elemento anterior
+                street_corners[i - 1][2] = record[0]
+                street_corners[i - 1][3] = record[1]
+            # Si el nombre de la calle del elemento actual es diferente al nombre de la calle del elemento anterior
+            else:
+                # Deja las coordenadas del elemento anterior sin cambiar
+                pass
 
     # Imprimir la lista en un archivo CSV
     with open("calles_culiacan.csv", "w", newline="", encoding="utf-8") as csv_file:
